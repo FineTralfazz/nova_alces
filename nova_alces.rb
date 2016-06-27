5d4f2dc4b720d5befd7484c00eae008b6a57ae60
Chris Bailey <chris@chrisbailey.io>
2016-06-27 01:42:25 -0800

    Support for multiple repos

--- nova_alces.rb
+++ nova_alces.rb
@@ -3,6 +3,7 @@ require 'bundler'
 require_relative 'config.rb'
 Bundler.require :default
 
+
 Dir.foreach 'steps' do |step|
   if step.end_with? '.rb'
     require_relative('steps/' + step)
@@ -10,25 +11,32 @@ Dir.foreach 'steps' do |step|
 end
 
 
-def run_steps(env)
-  dir = DIRS[env]
-  Dir.chdir dir
-  git_pull
-  build_superstar
-  restart_service SERVICES[env]
+Dir.foreach 'repos' do |repo|
+  if repo.end_with? '.rb'
+    require_relative('repos/' + repo)
+  end
 end
 
 
 post '/github_hook' do
   digest = OpenSSL::Digest.new 'sha1'
   hmac = OpenSSL::HMAC.digest digest, GITHUB_WEBHOOK_SECRET, request[:payload]
-#  if headers['X-Hub-Signature'] == hmac
-    response = JSON.parse request[:payload]
-    puts response
+  if headers['X-Hub-Signature'] == hmac
+    response = JSON.parse request[:payload], {:symbolize_names => true}
     env = REFS.key response[:ref]
+    repo = response[:repository][:name]
     if env
-      run_steps env
+      case repo
+      when 'robotmoose'
+        robotmoose env
+      else
+        puts "Unknown repo: #{ repo }"
+      end
+    else
+      puts "Unknown env: #{ env }"
     end
- # end
+  else
+    puts "Invalid HMAC"
+  end
 end
 
---
+++ repos/robotmoose.rb
@@ -0,0 +1,7 @@
+def robotmoose(env)
+  dir = DIRS[env]
+  Dir.chdir dir
+  git_pull
+  build_superstar
+  restart_service SERVICES[env]
+end
