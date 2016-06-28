6ff9060c5860e2cf391e8f19670ab209e0d8aa2e
Chris Bailey <chris@chrisbailey.io>
2016-06-27 18:22:04 -0800

    Fix style

--- nova_alces.rb
+++ nova_alces.rb
@@ -3,26 +3,19 @@ require 'bundler'
 require_relative 'config.rb'
 Bundler.require :default
 
-
 Dir.foreach 'steps' do |step|
-  if step.end_with? '.rb'
-    require_relative('steps/' + step)
-  end
+  require_relative('steps/' + step) if step.end_with? '.rb'
 end
-
 
 Dir.foreach 'repos' do |repo|
-  if repo.end_with? '.rb'
-    require_relative('repos/' + repo)
-  end
+  require_relative('repos/' + repo) if repo.end_with? '.rb'
 end
-
 
 post '/github_hook' do
   digest = OpenSSL::Digest.new 'sha1'
   hmac = OpenSSL::HMAC.digest digest, GITHUB_WEBHOOK_SECRET, request[:payload]
   if headers['X-Hub-Signature'] == hmac
-    response = JSON.parse request[:payload], {:symbolize_names => true}
+    response = JSON.parse request[:payload], symbolize_names: true
     env = REFS.key response[:ref]
     repo = response[:repository][:name]
     if env
@@ -30,17 +23,16 @@ post '/github_hook' do
       when 'robotmoose'
         robotmoose env
       else
-        halt 400, "Unknown repo: #{ repo }"
+        halt 400, "Unknown repo: #{repo}"
       end
     else
-      halt 400, "Unknown env: #{ env }"
+      halt 400, "Unknown env: #{env}"
     end
   else
-    halt 401, "Invalid HMAC"
+    halt 401, 'Invalid HMAC'
   end
 end
 
 get '/' do
-  "Nova Alces is running."
+  'Nova Alces is running.'
 end
-
